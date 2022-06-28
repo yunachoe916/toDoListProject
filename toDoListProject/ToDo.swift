b3f0c1ea62a1eafa4b4bbd946fa2f81f003f460c
Yuna Choe <yunachoe916@gmail.com>
2022-06-28 08:37:21 -0700

    Initial Commit

---
+++ toDoListProject/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoListProject
+//
+//  Created by Yuna Choe on 6/27/22.
+//
+
+import UIKit
+
+class ToDo{
+    var name = ""
+    var important = false
+}
---
+++ toDoListProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListProject
+//
+//  Created by Yuna Choe on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
